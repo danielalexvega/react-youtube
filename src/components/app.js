f46d0ee768e4d2896fe3e37e93f4aba99eb1c2f9
Daniel Vega <danielalexvega@gmail.com>
2020-09-15 22:04:26 -0500

    added style and VideoItem.css

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 import youtube from '../apis/youtube';
 
 const KEY = 'AIzaSyDNwVYR39VhH_Azb706OHj7OhniTSOBQDY';
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params:{
+        q: term,
         part: 'snippet',
         type: 'video',
         maxResults: 5,
         key: KEY
       }
     });
-  };
+
+  this.setState({ videos: response.data.items }); 
+};
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   };
