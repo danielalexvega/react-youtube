326739effd5668d1afde14eadc64a27f6f26aa20
Daniel Vega <danielalexvega@gmail.com>
2020-09-18 14:27:46 -0500

    Updates

---
+++ precache-manifest.8f587d741d248b201a9711c5746537ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a5704f14a0b34d931db4dff497638a",
+    "url": "/react-youtube/index.html"
+  },
+  {
+    "revision": "38a7b1fe255bb3358155",
+    "url": "/react-youtube/static/css/main.ca161dc0.chunk.css"
+  },
+  {
+    "revision": "691b69de9730a0750811",
+    "url": "/react-youtube/static/js/2.c48e0a4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube/static/js/2.c48e0a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a7b1fe255bb3358155",
+    "url": "/react-youtube/static/js/main.6cc7a11b.chunk.js"
+  },
+  {
+    "revision": "45f421ded0137d397bc0",
+    "url": "/react-youtube/static/js/runtime-main.00b2677d.js"
+  }
+]);
